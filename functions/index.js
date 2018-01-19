f0e32e52accf2c2e808c0a7b9e156ab0db56df7d
Pierre Castex <pierrecastex@mbp-de-pierre.home>
2018-01-19 17:14:12 +0100

    coucou

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,6 @@ process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 
-
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./service.json");
@@ -18,10 +17,10 @@ admin.initializeApp({
 var db = admin.database();
 
 // a. the action name from the make_name Dialogflow intent
-const NAME_ACTION = 'make_coeff';
+const NAME_ACTION = 'tide_coeff';
 // b. the parameters that are parsed from the make_name intent
 const DATE_ARGUMENT = 'date-time';
-const CITY_ARGUMENT = 'geo-city-fr';
+const CITY_ARGUMENT = 'port';
 
 exports.sillyNameMaker = functions.https.onRequest((request, response) => {
 	const app = new App({
@@ -36,35 +35,64 @@ exports.sillyNameMaker = functions.https.onRequest((request, response) => {
 	function makeName(app) {
 		let date = app.getArgument(DATE_ARGUMENT);
 		let city = app.getArgument(CITY_ARGUMENT);
+		console.log(app);
 		console.log(date);
 		console.log(city);
-		var ref = db.ref("ports/"+city+"/days");
-		db.ref("ports/").remove();
-		var refPorts = db.ref("ports/");
-		refPorts.on("value", function(snapshot) {
-			console.log(snapshot.val());
-			console.log(snapshot.key);
+		var ref = db.ref("ports1/"+city+"/days");
+		// // db.ref("ports/").remove();
+		// var ref2 = db.ref("ports2/");
+		// ref2.orderByChild("port").startAt(city.substring(0,3)).once("value", function(snapshot) {
+		// 	console.log(snapshot.val());
+		// 	console.log(snapshot.key);
+		// 	snapshot.forEach(function(childSnapshot) {
+		// 		snapshot.forEach(function(childSnapshot) {
+		// 	}
+		//
+		// });
 
-		});
+		var refPort = db.ref("entries").orderByChild("value").equalTo(newDate).once("value", function(snap) {
+
+				snap.forEach(function(chSnap) {
+		 			console.log(chSnap.val());
+					var rawCity = chSnap.val().synonyms[0];
+					console.log(newDate);
+					ref.orderByChild("day").equalTo(newDate).once("value", function(snapshot) {
+
+						snapshot.forEach(function(childSnapshot) {
+						console.log("titi");
+						console.log(childSnapshot.key);
+						console.log(childSnapshot.val().coeff);
+						var coeffs = childSnapshot.val().coeff
+						if ( coeffs.length == 1){
+							app.tell('Le coefficient de marée pour '+rawCity +' sera de '+coeffs[0]);
+						} else {
+							app.tell('Le coefficient de marée pour '+ rawCity+' sera de '+coeffs[0]+' en première partie de journée, puis de '+coeffs[1]+' par la suite');
+						}
+
+						var hours = childSnapshot.val().hours
+						var val1 = [[hours[0,0],hours[0,1]],[hours[2,0],hours[2,1]]];
+						var val2 = [[hours[1,0],hours[1,1]]];
+						if ( hours.length == 4){
+							val2.push([hours[3,0],hours[1,1]]);
+						}
+						var basseMer = (val1[0][1]<val2[0][1]) ? val1 : val2;
+						var pleineMer=  (val1[0][1]<val2[0][1]) ? val2 : val1;
+
+						app.tell('La mer sera basse à '+basseMer[0][0]+' pour une hauteur de '+basseMer[0][1]+' mètres');
+						if(basseMer.length = 2){
+						app.tell(', puis à '+basseMer[0][0]+' pour une hauteur de '+basseMer[0][1]+' mètres');
+						}
+
+						app.tell('La mer sera haute à '+pleineMer[0][0]+' pour une hauteur de '+pleineMer[0][1]+' mètres');
+						if(pleineMer.length = 2){
+						app.tell(', puis à '+pleineMer[0][0]+' pour une hauteur de '+pleineMer[0][1]+' mètres');
+						}
+					 // ...
+			 		});
+				});
+			});
 
 
-		var newDate = date.replace(/-/g ,"");
-		console.log(newDate);
-		ref.orderByChild("day").equalTo(newDate).once("value", function(snapshot) {
-
-			snapshot.forEach(function(childSnapshot) {
-			console.log("titi");
-	 	console.log(childSnapshot.key);
-	  console.log(childSnapshot.val().coeff);
-		var coeffs = childSnapshot.val().coeff
-		if ( coeffs.length == 1){
-			app.tell('Le coefficient de marée pour '+city +' sera de '+coeffs[0]);
-		} else {
-			app.tell('Le coefficient de marée pour '+ city+' sera de '+coeffs[0]+' en première partie de journée, puis de '+coeffs[1]+' par la suite');
-		}
-		console.log("titi");
-	 // ...
- });
 		  // console.log(snapshot.val());
 			//   console.log(snapshot.key());
 			// console.log("titi");
